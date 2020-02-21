7eb65266d459a711af180bd961222b547f4cb805
Chathu Vishwajith <4evercreations@gmail.com>
2020-02-21 19:26:09 +0530

    Add basic index.js

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const setupServer = () => {
+  const app = express();
+  const port = 3000;
+
+  app.get('/', (req, res) => {
+    res.send('Hello World!');
+  });
+
+  app.listen(port, () => {
+    console.log(`listening on port ${port}`);
+  });
+};
+
+setupServer();
